f2a918701fdfa99cd5b016988d500daa6e3e284a
JonathanZhang020706 <3145078758@qq.com>
2020-10-10 23:39:40 +0800

    new exercises
    none

---
+++ Chapter 2/Exc2.21/Exc2.21/Exc2.21.cpp
@@ -0,0 +1,24 @@
+﻿// Exc2.21.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/*
+    Date:2020-10-10
+    Author:Jonathan Zhang
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+//function main begins the program execution
+int main()
+{
+    puts("*********           ***                  *                  *");
+    puts("*       *         *     *               ***                * *");
+    puts("*       *        *       *             *****              *   *");
+    puts("*       *        *       *               *               *     *");
+    puts("*       *        *       *               *              *       *");
+    puts("*       *        *       *               *               *     *");
+    puts("*       *        *       *               *                *   *");
+    puts("*       *         *     *                *                 * *");
+    puts("*********           ***                  *                  *");
+
+    return 0;
+}
---
+++ Chapter 2/Exc2.22/Exc2.22/Exc2.22.cpp
@@ -0,0 +1,16 @@
+﻿// Exc2.22.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/*
+	Date:2020-10-10
+	Author:Jonathan Zhang
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+//function main begins the program execution
+int main()
+{
+	printf("*\n**\n***\n****\n*****\n");	//test what this statement prints
+
+	return 0;
+}
