14313d931c0771bb803e13cce624df4040f5c858
JonathanZhang020706 <3145078758@qq.com>
2020-10-15 14:54:16 +0800

    New Exercise
    Linloir

---
+++ inClass Exercise/Oct.15.1/Oct.15.1/Oct.15.1.cpp
@@ -0,0 +1,21 @@
+﻿// Oct.15.1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+
+int main()
+{
+    double pi = 3.1415926;
+    double r = 3.5, 
+           h = 4;
+    double s1, s2, s;
+
+    s1 = pi * r * r;
+    s2 = 2.0 * pi * r * h;
+    s = 2.0 * s1 + s2;
+    
+    printf("Area = %f\n", s);
+
+    return 0;
+}
+
