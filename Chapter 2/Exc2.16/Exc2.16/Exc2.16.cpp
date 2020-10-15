ab45868a925734ccb6019f0b4fa39ab86a4be4cc
JonathanZhang020706 <3145078758@qq.com>
2020-10-15 14:36:50 +0800

    New Exercise
    Linloir

---
+++ Chapter 2/Exc2.16/Exc2.16/Exc2.16.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int integer1, integer2;
+
+    printf("Please enter the first integer: ");
+    scanf_s("%d", &integer1);
+    printf("Please enter the second integer: ");
+    scanf_s("%d", &integer2);
+
+    int sum, mtp, sub, mod;     //the sum, multiplication, substraction and mod of the two integers
+    double div;     //the division of the two integers
+
+    sum = integer1 + integer2;
+    mtp = integer1 * integer2;
+    sub = integer1 - integer2;
+    mod = integer1 % integer2;
+
+    double num1 = integer1, num2 = integer2;    //Convert integer1 and integer2 into double
+    div = num1 / num2;
+
+    printf("The sum is : %d\n", sum);
+    printf("The multiplication is : %d\n", mtp);
+    printf("The substraction is : %d\n", sub);
+    printf("The division is : %f\n", div);
+    printf("The mod is : %d\n", mod);
+
+    return 0;
+}
+
---
+++ Chapter 2/Exc2.23/Exc2.23/Exc2.23.cpp
@@ -0,0 +1,33 @@
+﻿// Exc2.23.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/**
+*	Read 5 integers, find out and print the maxium and minimum one
+*	@author Linloir
+*	@author Jonathan Zhang
+*	@version 1.0 2020-10-13
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+	int integer1, integer2, integer3, integer4, integer5;	//int 5 integers
+
+	puts("%s" , "Enter 5 different integers and seperate them with space: ");
+	scanf_s("%d %d %d %d %d", &integer1， & integer2, &integer3, &integer4, &integer5);	//read the 5 integers
+
+	int max, min;
+	if (integer1 > integer2) {
+		max = integer1;
+		min = integer2;
+	}
+	else {
+		max = integer2;
+	}
+	if (integer3 > max) {
+		max = integer3;
+	}
+	else {
+		if(integer)
+	}
+}
+
