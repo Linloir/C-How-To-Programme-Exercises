5add61e377333f82ec02cb1178f133aa5d6af24d
JonathanZhang020706 <3145078758@qq.com>
2020-10-16 14:46:57 +0800

    New Exercise
    Linloir

---
+++ Chapter 2/Exercise 2.32/Exercise 2.32/Exercise 2.32.cpp
@@ -0,0 +1,39 @@
+﻿// BMI Counter Program
+/*
+    Date:   2020-10-15
+    Author: Linloir
+    Author: Jonathan Zhang
+    Version:1.0
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    double wt;  //weight of the user
+    double ht;  //height of the user
+    double bmi; //BMI of the user
+
+    printf("Please enter your weight (in kilogram) : ");
+    scanf_s("%lf", &wt);     //Read the weight of the user
+
+    printf("Please enter your height (in meter) : ");
+    scanf_s("%lf", &ht);     //Read the height of the user
+
+    bmi = wt / (ht * ht);
+
+    if (bmi < 18.5) {
+        printf("Your BMI value is %.1lf, which is considered UNDERWEIGHT. Beware!\n", bmi);
+    }
+    else if (bmi < 25) {
+        printf("Your BMI value is %.1lf, which is considered normal, good job!\n", bmi);
+    }
+    else if (bmi < 30) {
+        printf("Your BMI value is %.1lf, which is considered OVERWEIGHT. Beware!\n", bmi);
+    }
+    else {
+        printf("Your BMI value is %.1lf, which is considered OBESE. Beware!\n", bmi);
+    }
+
+    return 0;
+}
---
+++ inClass Exercise/Oct.16.1/Oct.16.1/Oct.16.1.cpp
@@ -0,0 +1,38 @@
+﻿// Oct.16.1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// A program that can solve a binary twice equation
+/*
+	Date:	2020-10-16
+	Author:	Jonathan Zhang
+	Author:	Linloir
+	Version:1.0
+*/
+
+#include <stdio.h>
+
+float sol(float, float);
+
+int main() {
+	puts("This is a program for solving the one-order equation");
+	puts("a * x = b");
+
+	printf("Enter a and b: ");
+	float a, b;
+	scanf_s("%f %f", &a, &b);
+
+	printf("For equation %g * x = %g\n", a, b);
+	printf("The solution of the equation is x = %g\n", sol(a, b));
+
+	return 0;
+}
+
+float sol(float a, float b) {
+	//ax = b
+	float x = b / a;
+
+	return x;
+}
+
+/*
+Comments:
+	1. Using "%g" can avoid the useless "0" in float numbers without losing precision due to "%.xf" rounds the number.
+*/
