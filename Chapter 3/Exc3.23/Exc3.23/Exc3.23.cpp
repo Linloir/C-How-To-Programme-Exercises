dae313e887534a2776e2576b92a1e13cedaf954a
JonathanZhang020706 <3145078758@qq.com>
2020-10-20 17:18:10 +0800

    New Exercise
    Linloir

---
+++ Chapter 3/Exc3.23/Exc3.23/Exc3.23.cpp
@@ -0,0 +1,26 @@
+﻿//:Find the max number among 10 positive numbers
+/*
+	Author:		Linloir(Jonathan Zhang)
+	Version:	1.0
+	Date:		2020-10-20
+*/
+
+#include <stdio.h>
+#include <Windows.h>		//in order to use clr
+
+int main() {
+	int number;
+	int largest = 0;
+	for (int counter = 1; counter <= 10; counter++) {
+		system("cls");		//clear screen
+
+		printf("largest number til now is: %d\n", largest);
+		printf("Enter number %d: ", counter);
+		scanf_s("%d", &number);
+
+		largest = number > largest ? number : largest;
+	}
+	printf("largest number is: %d", largest);
+
+	return 0;
+}
