ac7f315bc9047838b02ecf89bebb29cb3cdd1bc6
JonathanZhang020706 <3145078758@qq.com>
2020-10-23 15:41:31 +0800

    New Exercise
    Linloir

---
+++ inClass Exercise/find the number of Chicken and Rabbits/find the number of Chicken and Rabbits/find the number of Chicken and Rabbits.cpp
@@ -0,0 +1,28 @@
+﻿//:Known that number of chicken and rabbits in total is n, number of legs in total is m.
+// find the number of chicken and rabbits with two inputs 'n' and 'm'
+
+/*
+	Author:		Linloir(Jonathan Zhang)
+	Date:		2020-10-23
+*/
+
+#include <stdio.h>
+
+int main() {
+	int ckCount, rbCount;	//ckCount: number of chicken; rbCount: number of rabbits
+	int n, m;
+
+	printf("Please input n and m: ");
+	scanf_s("%d %d", &n, &m);
+
+	ckCount = (4 * n - m) % 2 == 0 ? (4 * n - m) / 2 : -1;
+	rbCount = n - ckCount;
+
+	if (ckCount >= 0 && rbCount >= 0)
+		printf("Number of chicken is %d, number of rabbits is %d\n", ckCount, rbCount);
+	else
+		printf("No solution!\n");
+
+	return 0;
+
+}
---
+++ inClass Exercise/rank 3 numbers/rank 3 numbers/rank 3 numbers.cpp
@@ -0,0 +1,29 @@
+﻿//:Using exchange method to rank three numbers* (ADVANCED TUTORIAL)
+/*
+	Author:		Linloir(Jonathan Zhang)
+	Date:		2020-10-23
+	Version:	1.0
+*/
+
+#include <stdio.h>
+
+int main() {
+	//Claim a stack of 3 numbers
+	int input[3];
+
+	//Require input
+	printf("Please input 3 numbers (type int): ");
+	scanf_s("%d %d %d", &input[0], &input[1], &input[2]);
+
+	//Exchange Method*
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3 - i - 1; j++) {
+			if (input[j] > input[j + 1]) {
+				int temp;
+				temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+			}
+		}
+	}
+}
