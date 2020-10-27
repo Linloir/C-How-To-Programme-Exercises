b91a60b1463336242b88bbf869daf55216872c27
JonathanZhang020706 <3145078758@qq.com>
2020-10-27 15:05:25 +0800

    New Exercise
    Linloir

---
+++ Chapter 4/Exc4.27/Exc4.27/Exc4.27.cpp
@@ -0,0 +1,39 @@
+﻿//:Find all Pythagorean triples for side1, side2, and the hypotenuse all no larger than 500.
+// Use a triple - nested for loop that simply tries all possibilities
+
+/*
+	Author:	Linloir(Jonathan Zhang)
+	Date:	2020-10-27
+	Version:1.0
+*/
+
+#include <stdio.h>
+
+int pow(int, int);
+
+int main() {
+	puts("*********Pythagorean triples finder*********");
+
+	int side1, side2, hypotenuse;
+	for (side1 = 1; side1 <= 500; side1++) {
+		for (side2 = 1; side2 <= 500; side2++) {
+			for (hypotenuse = 1; hypotenuse <= 500; hypotenuse++) {
+				if (pow(hypotenuse, 2) == pow(side1, 2) + pow(side2, 2))
+					printf("Pythagorean triples find! ( Side 1: %d;\tSide 2: %d;\thypotenuse: %d )\n", side1, side2, hypotenuse);
+			}
+		}
+	}
+
+	return 0;
+}
+
+int pow(int integer, int p) {
+	for (int step = 1; step < p; step++)
+		integer = integer * integer;
+	return integer;
+}
+
+/*
+Comments:
+	1. This program do not automatically ignore the repetitive data. ( e.g. 3,4,5 & 4,3,5 )
+*/
--- inClass Exercise/rank 3 numbers/rank 3 numbers/rank 3 numbers.cpp
+++ inClass Exercise/rank 3 numbers/rank 3 numbers/rank 3 numbers.cpp
@@ -5,6 +5,8 @@
 	Version:	1.0
 */
 
+/* The original version
+
 #include <stdio.h>
 
 int main() {
@@ -16,9 +18,9 @@ int main() {
 	scanf_s("%d %d %d", &input[0], &input[1], &input[2]);
 
 	//Exchange Method*
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3 - i - 1; j++) {
-			if (input[j] > input[j + 1]) {
+	for (int i = 0; i < 3; i++) {					//Counter of the BIGGEST numbers ranked
+		for (int j = 0; j < 3 - i - 1; j++) {		//location of bit to be ranked next
+			if (input[j] > input[j + 1]) {			//Exchange if bigger
 				int temp;
 				temp = input[j];
 				input[j] = input[j + 1];
@@ -26,4 +28,51 @@ int main() {
 			}
 		}
 	}
+
+	//Output result
+	for (int i = 0; i < 3; i++) {
+		printf("%d ", input[i]);
+	}
+
+	return 0;
+}
+
+*/
+
+//The program can be extend to rank arbitrarily counts of numbers:
+
+#include <stdio.h>
+
+int main() {
+	//Definite the count of numbers
+	int count;
+	printf("Please enter the count of numbers you want to sort (of type int): ");
+	scanf_s("%d", &count);
+
+	//Initialize the stack of numbers
+	
+
+	//Store input numbers into stack
+	for (int step = 0; step < count; step++) {
+		printf("Enter number %d: ", step + 1);
+		scanf_s("%d", &input[step]);
+	}
+
+	//Exchange method (body part)
+	for (int rkdCount = 0; rkdCount < count; rkdCount++) {
+		for (int pos = 0; pos < count - rkdCount - 1; pos++) {
+			if (input[pos] > input[pos + 1]) {
+				int temp = input[pos];
+				input[pos] = input[pos + 1];
+				input[pos + 1] = temp;
+			}
+		}
+	}
+
+	//Output
+	for (int step = 0; step < count; step++) {
+		printf("%d ", input[step]);
+	}
+
+	return 0;
 }
