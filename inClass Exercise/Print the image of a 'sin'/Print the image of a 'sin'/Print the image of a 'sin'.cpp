e6f1720755476646e7fbeb58743b63f109c566dd
JonathanZhang020706 <3145078758@qq.com>
2020-10-31 22:21:06 +0800

    Update
    Linloir

---
+++ inClass Exercise/Print the image of a 'sin'/Print the image of a 'sin'/Print the image of a 'sin'.cpp
@@ -0,0 +1,21 @@
+﻿//:Print the image of a 'sin' with one printf("*") statement
+
+/*
+	Author:	Linloir(Jonathan Zhang)
+	Date:	2020-10-30
+	Version:1.0
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int intervalBetweenMaxAndMin = 10;	//set default interval as 10;
+	printf("Please enter the interval between max and min (for precision): ");
+	scanf_s("%d", &intervalBetweenMaxAndMin);
+
+	//set the value difference between two lines
+	double valueEachLine = 1 / intervalBetweenMaxAndMin;
+
+
+}
